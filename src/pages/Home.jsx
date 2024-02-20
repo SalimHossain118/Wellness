4000d50cb1d0dc3bbf3384fd78ab8119352b4e1a
vaskar-ghosh <vaskar.ghosh.99.23@gmail.com>
2024-02-20 17:09:26 +0600

    banner section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 /** @format */
 
 import React from "react";
+import Category from "../components/Category";
+import Banner from "../components/Banner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+      <Category />
+    </div>
+  );
 };
 
 export default Home;
